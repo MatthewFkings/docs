571d35fd08fc59c7ecd65fd577f892f28c1f878a
Mateo <mpereiravillamil@danielcastelao.org>
2023-11-20 09:33:24 +0100

    ejercicio finalizado

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Xogador1","l":"amosarDato()"},{"p":"<Unnamed>","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Xogador1","l":"Xogador1()","u":"%3Cinit%3E()"}];updateSearchResults();
